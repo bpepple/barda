5ac1ed874175961f51a8c6dd292d6e412007cb0f
Brian Pepple <bdpepple@gmail.com>
2022-12-23 14:14:10 -0500

    Initial settings module

---
+++ barda/settings.py
@@ -0,0 +1,62 @@
+"""Class to handle project settings"""
+
+import configparser
+import platform
+from os import environ
+from pathlib import Path, PurePath
+from typing import Optional
+
+from xdg.BaseDirectory import save_config_path
+
+
+class BardaSettings:
+    """Class to handle project settings"""
+
+    @staticmethod
+    def get_settings_folder() -> Path:
+        """Method to determine where the users settings should be saved"""
+
+        if platform.system() != "Windows":
+            return Path(save_config_path("barda"))
+
+        windows_path = PurePath(environ["APPDATA"]).joinpath("Barda")
+        return Path(windows_path)
+
+    def __init__(self, config_dir: Optional[str] = None) -> None:
+        self.metron_user: str = ""
+        self.metron_password: str = ""
+
+        self.config = configparser.ConfigParser()
+
+        folder = Path(config_dir) if config_dir else BardaSettings.get_settings_folder()
+        self.settings_file = folder / "settings.ini"
+
+        if not self.settings_file.parent.exists():
+            self.settings_file.parent.mkdir()
+
+        # Write the config file if it doesn't exist
+        if not self.settings_file.exists():
+            self.save()
+        else:
+            self.load()
+
+    def load(self) -> None:
+        """Method to retrieve a users settings."""
+        self.config.read(self.settings_file)
+
+        if self.config.has_option("metron", "user"):
+            self.metron_user = self.config["metron"]["user"]
+
+        if self.config.has_option("metron", "password"):
+            self.metron_password = self.config["metron"]["password"]
+
+    def save(self) -> None:
+        """Method to save a users settings"""
+        if not self.config.has_section("metron"):
+            self.config.add_section("metron")
+
+        self.config["metron"]["user"] = self.metron_user
+        self.config["metron"]["password"] = self.metron_password
+
+        with self.settings_file.open("w") as configfile:
+            self.config.write(configfile)

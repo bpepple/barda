a20dcd0ab6da7a824846d55b2047f3741bf7971a
Brian Pepple <bdpepple@gmail.com>
2022-12-23 14:36:32 -0500

    Add comic path to settings

--- barda/settings.py
+++ barda/settings.py
@@ -25,6 +25,7 @@ class BardaSettings:
     def __init__(self, config_dir: Optional[str] = None) -> None:
         self.metron_user: str = ""
         self.metron_password: str = ""
+        self.path: str = ""
 
         self.config = configparser.ConfigParser()
 

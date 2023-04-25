ce65f965b7ecd552c46f0e06885d96fb81244c16
Brian Pepple <bdpepple@gmail.com>
2023-04-25 16:34:51 -0400

    Move ignore enums to separate module

--- barda/cv_importer.py
+++ barda/cv_importer.py
@@ -20,6 +20,7 @@ from barda.base_importer import BaseImporter
 from barda.exceptions import ApiError
 from barda.gcd.db import DB
 from barda.gcd.gcd_issue import GCD_Issue, Rating
+from barda.ignore_resources import Ignore_Characters, Ignore_Creators, Ignore_Teams
 from barda.image import CVImage
 from barda.resource_keys import ResourceKeys, Resources
 from barda.settings import BardaSettings
@@ -51,113 +52,6 @@ class CV_Creator(Enum):
     Axel_Alonso = 23115
     Jim_Shooter = 40450
     Mike_Richardson = 45055
-
-
-@unique
-class Ignore_Characters(Enum):
-    Abraham_Lincoln = 14131
-    Adolf_Hitler = 9671
-    Andy_Handy = 63039
-    Arjuna = 26052
-    Barack_Obama = 56661
-    Bebe = 48007
-    Benjamin_Franklin = 38634
-    Bill_Clinton = 11570
-    Black_Knight = 92951  # Scooby Doo
-    Captain_Cutler = 92946  # Scooby Doo
-    Casey_the_Cop = 51046
-    Charlie_Funland_Robot = 92958
-    Chief_Hot_Foot = 47651
-    Clancy = 18387
-    Conquest = 27703
-    Daniel_Boone = 67377
-    Death = 9632
-    Del_Close = 96033
-    Doctor_Jekyll = 22215
-    Donald_Trump = 17028
-    Famine = 22657
-    Frank_Belknap_Long = 142839
-    Frankensteins_Monster = 38840
-    Franklin_D_Roosevelt = 31595
-    Genghis_Khan = 37303
-    George_H_W_Bush = 9957
-    George_W_Bush = 4660
-    George_Washington = 36452
-    Ghost_Clown = 92950
-    H_P_Lovecraft = 10641
-    Handy_Andy = 168137
-    Headless_Horseman = 53680
-    Homer_DC = 20548
-    Honey_in_Hollywood = 48091
-    Jack_Kirby = 11432
-    Jasper = 22491
-    Jerry_Jitterbug = 47403
-    John_Ostrander = 96034
-    Jordan_White = 170634
-    Krishna = 44322
-    L_Ron_Hubbard = 96035
-    Lash_LaRue = 75444
-    Little_Pete = 47652
-    Little_Pocahontas = 48088
-    Lyndon_Johnson = 38601
-    Merlyn = 14820
-    Nelson_Mandela = 19799
-    Olga = 32176
-    P_W_Botha = 173183
-    Peg_DC = 18142
-    Private_Pete = 38858
-    Professor_Doolittle = 63117
-    Rudolf_Hess = 35217
-    Sagebrush_Sam = 48154
-    Santa_Claus = 22143
-    Scruffy_the_Tramp = 49296
-    Shorty = 90520
-    Smokey_the_Bear = 8485
-    Snow_Ghost = 92954  # Scooby Doo
-    Sonia_Haft_Greene = 142844
-    Spooky_Space_Kook = 92947
-    Stan_Lee = 3115
-    Sunshine_Nelly = 174071
-    Titania = 21031
-    The_Werewolf = 92953  # Scooby Doo
-    Tom_DeFalco = 11901
-    Varsity_Vic = 47460
-    Volto = 115733
-    War = 13083
-    Wax_Phantom = 92957
-    William_Shakespeare = 39377
-    Witch_Doctor = 92959  # Scooby Doo
-    Willy_DC = 34761
-    Wolf_Man = 40493
-
-
-@unique
-class Ignore_Creators(Enum):
-    Typeset = 67476
-    Robert_Simpson = 69288  # Lucasfilm editor
-
-
-@unique
-class Ignore_Teams(Enum):
-    Batman_and_Robin = 56496
-    British_Army = 56617
-    Cavemen = 57593
-    Communists = 56975
-    Dinosaurs = 56551
-    Father_and_Daughter = 57450
-    Gravediggers = 59742
-    Imperial_Japanese_Army = 56573
-    Imperial_Japanese_Army_2 = 56574
-    Justice_Forever = 57862
-    Mummies = 58219
-    Native_Americans = 59881
-    Pirates = 59715
-    Special_Air_Service = 57233
-    United_States_Air_Force = 44417
-    United_States_Navy = 44418
-    United_States_Special_Forces = 55045
-    Werecats = 61574
-    Zombies = 44930
 
 
 class ComicVineImporter(BaseImporter):

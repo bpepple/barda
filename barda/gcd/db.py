ed8501993af977915107292c8865917fd23d33ff
Brian Pepple <bdpepple@gmail.com>
2024-04-24 12:03:36 -0400

    Use sqlite for gcd database

--- barda/gcd/db.py
+++ barda/gcd/db.py
@@ -1,10 +1,5 @@
+import sqlite3
 from dataclasses import dataclass
-
-import mysql.connector
-import questionary
-from mysql.connector.errors import DatabaseError, InterfaceError
-
-from barda.styles import Styles
 
 
 @dataclass
@@ -23,8 +18,8 @@ class GcdReprintIssue:
 
 class DB:
     def __init__(self) -> None:
-        self.db = self._get_db()
-        self.cursor = self.db.cursor()
+        self.db: sqlite3.Connection = self._get_db()
+        self.cursor: sqlite3.Cursor = self.db.cursor()
 
     def __enter__(self):
         return self
@@ -33,29 +28,25 @@ class DB:
         self.cursor.close()
 
     @staticmethod
-    def _get_db():
-        try:
-            return mysql.connector.connect(
-                host="frodo",
-                user="bpepple",
-                passwd="123456",
-                database="gcd",
-            )
-        except (DatabaseError, InterfaceError) as e:
-            questionary.print(f"Database Error: {e}", style=Styles.ERROR)
-            exit(0)
+    def _get_db() -> sqlite3.Connection:
+        return sqlite3.connect("/home/bpepple/.cache/barda/gcd.db")
 
-    def get_series_list(self, name: str):  # sourcery skip: class-extract-method
+    def get_series_list(self, name: str) -> list[any]:  # sourcery skip: class-extract-method
         q = (
             "SELECT id, name, year_began, issue_count, publishing_format "
-            "from gcd_series WHERE country_id=225 AND name=%s "
+            "from gcd_series WHERE country_id=225 AND name=? "
             # "AND NOT publishing_format='collected edition' "
             # "AND NOT publishing_format='Collected Series'"
             # "AND NOT publishing_format='collected editions' "
             # "AND NOT publishing_format='coloring book' "
-            "ORDER BY year_began ASC;"
+            "ORDER BY year_began ASC"
         )
-        self.cursor.execute(q, [name])
+        self.cursor.execute(
+            q,
+            [
+                name,
+            ],
+        )
         return self.cursor.fetchall()
 
     def get_issues(self, series_id: int, issue_number: str):
@@ -63,45 +54,70 @@ class DB:
         if issue_number:
             q = (
                 "SELECT id, number, price, barcode, page_count, rating, indicia_publisher_id FROM "
-                "gcd_issue WHERE series_id=%s AND number=%s AND variant_of_id IS NULL;"
+                "gcd_issue WHERE series_id=? AND number=? AND variant_of_id IS NULL"
             )
             params.append(issue_number)
         else:
             q = (
                 "SELECT id, number, price, barcode, page_count, rating, indicia_publisher_id FROM "
-                "gcd_issue WHERE series_id=%s AND variant_of_id IS NULL;"
+                "gcd_issue WHERE series_id=? AND variant_of_id IS NULL"
             )
         self.cursor.execute(q, params)
         return self.cursor.fetchall()
 
-    def get_stories(self, issue_id: int):
-        q = "SELECT title from gcd_story WHERE type_id=19 AND issue_id=%s ORDER BY sequence_number;"
-        self.cursor.execute(q, [issue_id])
+    def get_stories(self, issue_id: int) -> list[any]:
+        q = "SELECT title from gcd_story WHERE type_id=19 AND issue_id=? ORDER BY sequence_number"
+        self.cursor.execute(
+            q,
+            [
+                issue_id,
+            ],
+        )
         return self.cursor.fetchall()
 
     def get_story_ids(self, issue_id: int) -> list[any]:
         """Return a list of story id's for the issue."""
-        q = "SELECT id from gcd_story WHERE type_id=19 AND issue_id=%s ORDER BY id;"
-        self.cursor.execute(q, [issue_id])
+        q = "SELECT id from gcd_story WHERE type_id=19 AND issue_id=? ORDER BY id"
+        self.cursor.execute(
+            q,
+            [
+                issue_id,
+            ],
+        )
         return self.cursor.fetchall()
 
     def get_reprints_ids(self, story_id: int) -> list[any]:
         """Returns a list of reprint issue id's for the story."""
-        q = "SELECT DISTINCT target_issue_id FROM gcd_reprint WHERE origin_id=%s;"
-        self.cursor.execute(q, [story_id])
+        q = "SELECT DISTINCT target_issue_id FROM gcd_reprint WHERE origin_id=?"
+        self.cursor.execute(
+            q,
+            [
+                story_id,
+            ],
+        )
         return self.cursor.fetchall()
 
     def get_reprint_issue(self, issue_id: int) -> GcdReprintIssue:
-        q = "SELECT series_id, number FROM gcd_issue WHERE id=%s;"
-        self.cursor.execute(q, [issue_id])
+        q = "SELECT series_id, number FROM gcd_issue WHERE id=?"
+        self.cursor.execute(
+            q,
+            [
+                issue_id,
+            ],
+        )
         series_id, number = self.cursor.fetchone()
         # Check that number is all digits.
         if str(number).isdigit():
             number = int(number)
         else:
             return GcdReprintIssue(issue_id, None, None, None)
-        q = "SELECT name, country_id, year_began, publication_type_id FROM gcd_series WHERE id=%s AND country_id=225;"
-        self.cursor.execute(q, [series_id])
+        q = "SELECT name, country_id, year_began, publication_type_id FROM gcd_series WHERE id=? AND country_id=225"
+        self.cursor.execute(
+            q,
+            [
+                series_id,
+            ],
+        )
         res = self.cursor.fetchone()
         if res is None:
             return GcdReprintIssue(issue_id, None, None, None)
